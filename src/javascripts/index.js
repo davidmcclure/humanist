8039ee7f880563421701383047de0d27de3cdc1f
David McClure <davidwilliammcclure@gmail.com>
2014-10-05 23:36:11 -0700

    Starting to block in the react components.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,4 +1,23 @@
 
 
-var d3 = require('d3-browserify');
-console.log(d3);
+/** @jsx React.DOM */
+var React = require('react');
+
+
+var Network = React.createClass({
+
+  /**
+   * The top-level SVG container.
+   */
+  render: function() {
+    return <svg id="network">{this.props.children}</svg>;
+  }
+
+});
+
+
+// Root component.
+var humanist = React.renderComponent(
+  <Network />,
+  document.getElementById('primary')
+);
