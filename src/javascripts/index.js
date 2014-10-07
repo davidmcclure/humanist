ddc48f46295dadb676d9b3f0ce2fc987d17df328
David McClure <davidwilliammcclure@gmail.com>
2014-10-07 12:40:16 -0700

    Basic node plotting working.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -13,17 +13,57 @@ request
     return [n.graphics.x, n.graphics.y];
   });
 
-  var svg = d3.select('#network');
+  // TODO: Always fullscreen?
+  var h = 1000;
+  var w = 1000;
 
-  svg
-    .selectAll('circle')
+  // X scale.
+  var x = d3.scale.linear()
+    .domain([0, w])
+    .range([0, w]);
+
+  // Y scale.
+  var y = d3.scale.linear()
+    .domain([0, h])
+    .range([h, 0]);
+
+  // Zoomable container.
+  var svg = d3.select('#primary')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .append('g')
+    .call(
+      d3.behavior.zoom()
+        .x(x)
+        .y(y)
+        .scaleExtent([0.1, 10])
+        .on('zoom', zoom)
+    );
+
+  // Overlay.
+  svg.append("rect")
+    .attr("class", "overlay")
+    .attr("width", w)
+    .attr("height", h);
+
+  // Nodes.
+  var nodes = svg.selectAll('circle')
     .data(nodes)
     .enter()
     .append('circle')
     .classed({ node: true })
-    .attr('r', 5)
-    .attr('transform', function(d) {
-      return 'translate('+d+')';
-    });
+    .attr('r', 2)
+    .attr('transform', transform);
+
+  // Zoom circles.
+  function zoom() {
+    nodes.attr('transform', transform);
+  };
+
+  // Position element.
+  function transform(d) {
+    return 'translate('+x(d[0])+','+y(d[1])+')';
+  };
 
 });
