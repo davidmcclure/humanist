17674e63bd06d3992839a2bdff7bd20264dd6922
David McClure <davidwilliammcclure@gmail.com>
2014-10-07 00:28:32 -0700

    Plotting point for nodes.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,3 +2,27 @@
 
 var d3 = require('d3-browserify');
 var request = require('superagent');
+var _ = require('lodash');
+
+
+request
+.get('data.json')
+.end(function(error, res) {
+
+  var data = _.map(res.body.nodes, function(n) {
+    return [n.graphics.x, n.graphics.y];
+  });
+
+  var svg = d3.select('#network');
+
+  svg
+    .selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('transform', function(d) {
+      return 'translate('+d+')';
+    });
+
+});
