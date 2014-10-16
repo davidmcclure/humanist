77f139db6d6b209fd6c35cb4f79045293b766eef
David McClure <davidwilliammcclure@gmail.com>
2014-10-16 12:50:08 -0700

    First pass on Grunt data preprocessing working.

---
+++ grunt/data.js
@@ -0,0 +1,74 @@
+
+
+var _ = require('lodash');
+var jf = require('jsonfile');
+var path = require('path');
+
+module.exports = function(grunt) {
+
+  grunt.registerTask(
+    'data',
+    'Preprocess the data file.',
+    function() {
+
+      // Read the data file.
+      var d1 = grunt.file.readJSON('data.json');
+      var d2 = {}; // New data.
+
+      // Data extent:
+      // ------------
+
+      d2.extent = {};
+
+      // Node X/Y coordinates.
+      var coords = _.map(d1.nodes, function(n) {
+        return [n.graphics.x, n.graphics.y];
+      });
+
+      // X and Y coordinates.
+      var xs = _.pluck(coords, 0);
+      var ys = _.pluck(coords, 1);
+
+      // X/Y min/max values.
+      d2.extent.xmin = _.min(xs);
+      d2.extent.xmax = _.max(xs);
+      d2.extent.ymin = _.min(ys);
+      d2.extent.ymax = _.max(ys);
+
+      // Deltas on X/Y axes.
+      d2.extent.dx = d2.xmax-d2.xmin;
+      d2.extent.dy = d2.ymax-d2.ymin;
+
+      // Nodes:
+      // ------
+
+      d2.nodes = {};
+
+      // Label -> node.
+      _.each(d1.nodes, function(n) {
+        d2.nodes[n.label] = n;
+      });
+
+      // Edges:
+      // ------
+
+      d2.edges = {};
+
+      // Label -> sibling labels.
+      _.each(d1.links, function(e) {
+
+        var s = d1.nodes[e.source].label;
+        var t = d1.nodes[e.target].label;
+
+        // Register the link.
+        if (_.has(d2.edges, s)) d2.edges[s].push(t);
+        else d2.edges[s] = [t];
+
+      });
+
+      var dist = path.resolve(__dirname, '../_site/data.json');
+      jf.writeFileSync(dist, d2);
+
+    });
+
+};
