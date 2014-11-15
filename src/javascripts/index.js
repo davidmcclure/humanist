78618734e73b5f5c7bd821019c3431d842fcc95a
David McClure <davidwilliammcclure@gmail.com>
2014-11-15 10:34:05 -0800

    Scrubs out the search module.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -8,7 +8,6 @@ Backbone.$ = $;
 var Network = require('./network/network.ctl');
 var Minimap = require('./minimap/minimap.ctl');
 var Timeline = require('./timeline/timeline.ctl');
-var Search = require('./search/search.ctl');
 var Router = require('./router');
 
 
@@ -17,7 +16,6 @@ $.getJSON('data.json', _.bind(function(data) {
   // Modules:
   new Minimap(data);
   new Timeline(data);
-  new Search(data);
   new Network(data);
 
   // Router:
